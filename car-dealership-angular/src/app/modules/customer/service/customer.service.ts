59d984cebb55f751046a81ccaf07569368428bbf
Akyy03 <akraaam003@gmail.com>
2024-05-22 15:55:11 +0300

    Customer dashboard

--- car-dealership-angular/src/app/modules/customer/service/customer.service.ts
+++ car-dealership-angular/src/app/modules/customer/service/customer.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Car } from '../../../models/car.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
+  private apiUrl = 'http://localhost:8080/cars';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getCars(): Observable<Car[]> {
+    return this.http.get<Car[]>(`${this.apiUrl}/all`);
+  }
 }
